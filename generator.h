e1b67aefaf95e0219276fb31b0198aa47a0e867f
Pmarcos <pmarcos4235@gmail.com>
2026-06-04 02:23:46 +0800

    Organização dos arquivos

--- generator.h
+++
@@ -1,7 +0,0 @@
-#ifndef GENERATOR_H
-#define GENERATOR_H
-#include "event.h"
-Event *gerar_eventos(int n);
-void   ordenar_por_valor(Event *dados, int n);
-void   inspecionar_eventos(const Event *eventos, int n, int max_print);
-#endif
